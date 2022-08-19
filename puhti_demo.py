9c512c6d0b261555e88f69b49e2deaab2637769c
Mikko Saukkoriipi <mikko.p.saukkoriipi@helsinki.fi>
2022-08-19 12:04:26 +0300

    Add Puhti demo script

---
+++ puhti_demo.py
@@ -0,0 +1,40 @@
+# Test what CPUs and GPUs you have booked for your batch job
+
+import torch
+import os
+
+def check_my_gpu():
+    """ Check if system has GPU available and print possible GPU details."""
+    gpu_available = torch.cuda.is_available()
+    if gpu_available:
+        print("\nGPU is available!")
+        number_of_gpus = torch.cuda.device_count()
+        print("Number of GPUs: {}".format(torch.cuda.device_count()))
+        print("GPU types:")
+        for i in range(number_of_gpus):
+            print("GPU {} type: {}".format(i, torch.cuda.get_device_name(i)))
+    else:
+        print("No GPUs available!")
+
+def check_my_cpu():
+    """ Check number of CPUs in partition and how many you are booking."""
+    print("\nTotal number of CPUs in the system: {}".format(os.cpu_count()))
+    print("You are booking {} CPUs.".format(len(os.sched_getaffinity(0))))
+
+def create_file():
+    """ Save simple text file to remember.txt."""
+    f = open("remember.txt", "w")
+    f.write("Remember following:\n\n")
+    f.write("Things you print will go to file defined in run_script\n")
+    f.write("In this example prints are saved to output.txt\n\n")
+    f.write("Saved results will be saved similarly as in normal computer\n")
+    f.write("Like this file is saved in remember.txt")
+    f.close()
+
+def main():
+    check_my_cpu()
+    check_my_gpu()
+    create_file()
+
+if __name__ == "__main__":
+    main()
--- run_script.sh
+++ run_script.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=d_query
-#SBATCH --account=project_2000745
-#SBATCH --reservation=summerschool-gpu
-#SBATCH --partition=gpu
-#SBATCH --time=00:05:00
+#SBATCH --job-name=hw_test
+#SBATCH --account=project_2006308
+#SBATCH --partition=gputest
+#SBATCH --time=00:00:30
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=4000
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4G
 #SBATCH --gres=gpu:v100:1
+#SBATCH --mail-type=END
 #SBATCH --output=output.txt
 
-srun ./copy2d
+module load pytorch
 
-## NOTES TO COMPILE AND LOAD GPU CODE:
-# module load nvhpc/21.9  nvhpc-mpi/openmpi-4.0.5
-# nvc -o hello hello.c -mp=gpu -gpu=cc70
+srun python3 puhti_demo.py
